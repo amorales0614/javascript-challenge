571c65cf0aba8d234288e159423bf6efabf6b404
Ana Morales <ammorales614@gmail.com>
2021-02-26 16:44:26 -0500

    commit 4

--- UFO Level 1/static/js/app.js
+++ UFO Level 1/static/js/app.js
@@ -6,3 +6,12 @@ console.log(tableData);
 var tableBody = d3.select("tbody");
 
 // getting values for each column
+tableData.forEach(function(ufos){
+	console.log(ufos);
+
+	// appending new row for each sighting
+	var nRow = tableBody.append("tr");
+
+	
+
+});
